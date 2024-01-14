11606271f1dc7c4ab1f6b86ecb97bd4da590682f
Minor Gordon <minorg@server.fake>
2024-01-14 17:57:27 -0500

    add tests

---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+import pdftotext
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data_dir_path() -> Path:
+    path_ = Path(__file__).absolute() / "data"
+    assert path_.is_dir()
+    return path_
+
+
+@pytest.fixture(scope="session")
+def pdf_file_paths(data_dir_path: Path) -> tuple[Path, ...]:
+    pdf_test_data_dir_path = data_dir_path / "pdf"
+    return tuple(
+        sorted(
+            pdf_test_data_dir_path / file_name
+            for file_name in os.listdir(pdf_test_data_dir_path)
+            if file_name.startswith("towndex-test-document-")
+            and file_name.endswith(".pdf")
+        )
+    )
+
+
+@pytest.fixture(scope="session")
+def pdf_texts(pdf_file_paths: tuple[Path, ...]) -> tuple[str, ...]:
+    result: list[str] = []
+    for pdf_file_path in pdf_file_paths:
+        with Path.open(pdf_file_path, "rb") as pdf_file:
+            pdf = pdftotext.PDF(pdf_file)  # type: ignore  # noqa: PGH003
+            text: str = "\n\n".join(pdf)  # type: ignore  # noqa: PGH003
+            result.append(text)
+    return tuple(result)
---
+++ tests/named_entity_recognizer_test.py
@@ -0,0 +1,25 @@
+import os
+
+import pytest
+
+from doc2sdo.models.llm_metadata import LlmMetadata, GPT_3_5
+from doc2sdo.named_entity_recognizer import NamedEntityRecognizer
+
+
+@pytest.mark.parametrize(
+    "model",
+    [("en_core_web_md",), (GPT_3_5,)],
+)
+def test_recognizer(model: LlmMetadata | str, pdf_texts: tuple[str, ...]) -> None:
+    if isinstance(model, LlmMetadata) and (
+        "OPENAI_API_KEY" not in os.environ or "OPENAI_API_ORG" not in os.environ
+    ):
+        return
+
+    sut = NamedEntityRecognizer(language="english", model=model)
+    for pdf_text in pdf_texts:
+        named_entities = tuple(sut.recognize(pdf_text))
+        # assert named_entities
+        for named_entity in named_entities:
+            assert named_entity.name.value
+            assert named_entity.uri
